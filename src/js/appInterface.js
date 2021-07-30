34fdb772d75dc9f2c1ae1fea8cebe605ec27e13d
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-07-30 18:08:32 +0300

    second commit

--- src/js/appInterface.js
+++ src/js/appInterface.js
@@ -14,6 +14,14 @@ export default class AppInterface {
         this.createPost(e);
       }
     });
+
+    const modalInput = document.querySelector('.form__input');
+    modalInput.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        this.createPostWithManualCoords(e);
+      }
+    });
   }
 
   markupPost(time, text, coordinates) {
@@ -34,7 +42,6 @@ export default class AppInterface {
     const text = e.target.value;
     const newCoordinates = await this.coordinates();
     const modal = document.querySelector('.modal');
-    const modalInput = document.querySelector('.form__input');
     let coordinates = '';
     let coordsLatitude = '';
     let coordsLongitude = '';
@@ -48,24 +55,24 @@ export default class AppInterface {
       e.target.value = '';
     } else {
       modal.classList.remove('hidden');
-      modalInput.addEventListener('keydown', (event) => {
-        if (event.keyCode === 13) {
-          event.preventDefault();
-          coordinates = modalInput.value;
-          if (this.validateCoordinates(coordinates)) {
-            modal.classList.add('hidden');
-            const newPost = this.markupPost(time, text, coordinates);
-            const tweetsContent = document.querySelector('.tweets__content');
-            tweetsContent.insertAdjacentHTML('afterbegin', newPost);
-            event.target.value = '';
-          } else {
-            alert('Enter the coordinates of the following type: 00.00000, 0.00000');
-          }
-        }
-      });
     }
+  }
 
-    e.target.value = '';
+  createPostWithManualCoords(e) {
+    const mainInput = document.querySelector('.footer__input');
+    const modal = document.querySelector('.modal');
+    const newDate = new Date();
+    const time = newDate.toLocaleString();
+    if (this.validateCoordinates(e.target.value)) {
+      const newPost = this.markupPost(time, mainInput.value, e.target.value);
+      const tweetsContent = document.querySelector('.tweets__content');
+      tweetsContent.insertAdjacentHTML('afterbegin', newPost);
+      modal.classList.add('hidden');
+      e.target.value = '';
+      mainInput.value = '';
+    } else {
+      alert('Enter the coordinates of the following type: 00.00000, 0.00000');
+    }
   }
 
   async coordinates() {
@@ -90,7 +97,7 @@ export default class AppInterface {
   }
 
   validateCoordinates(value) {
-    const templateRegExp = /^\[?([-+]?\d{1,2}[.]\d+),\s*([-+]?\d{1,3}[.]\d+)\]?$/gm;
-    return templateRegExp.test(value);
+    const regExp = /^\[?([-+]?\d{1,2}[.]\d+),\s*([-+]?\d{1,3}[.]\d+)\]?$/gm;
+    return regExp.test(value);
   }
 }
